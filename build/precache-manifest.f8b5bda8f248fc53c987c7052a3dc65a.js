234b74495fbfe86f481ee7691e3397a0b5d5e8f2
pankajdevbanyal <pankajdevbanyal@gmail.com>
2020-07-28 11:07:30 +0530

    initial commit

---
+++ build/precache-manifest.f8b5bda8f248fc53c987c7052a3dc65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131348862c0a707a67414c3b3c8550b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3b5fd64eedd306fb3ec",
+    "url": "/static/css/main.38411ed3.chunk.css"
+  },
+  {
+    "revision": "ebb47de3ef635d2979a7",
+    "url": "/static/js/2.18594650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18594650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b5fd64eedd306fb3ec",
+    "url": "/static/js/main.f39010fc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,36 @@
+
+process.env.NODE_ENV = "production";
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+const PORT = process.env.PORT || 5000 ;
+
+
+
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static(path.resolve(__dirname,'build')))
+
+    app.get('*',(req,res)=>{
+        return res.sendFile('/index.html');
+    });
+}
+
+
+
+
+app.get('/',(req,res)=>{
+   return res.send('<h1>HELLO BANYAL....</h1>')
+})
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server starte don PORT ${PORT}`)
+})
